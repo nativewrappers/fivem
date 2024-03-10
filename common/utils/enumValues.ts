3bd5180eba2b56d6a0c516c527c74e046e3ecbbe
Linden <65407488+thelindat@users.noreply.github.com>
2024-03-10 14:32:58 +1100

    chore(common): format with prettier

--- common/utils/enumValues.ts
+++ common/utils/enumValues.ts
@@ -11,13 +11,13 @@ export function enumValues<T extends string | number>(enumObj: {
 export function* enumValues<T>(enumObj: { [key: string]: T }): IterableIterator<T> {
 	let isStringEnum = true;
 	for (const property in enumObj) {
-		if (typeof enumObj[property] === "number") {
+		if (typeof enumObj[property] === 'number') {
 			isStringEnum = false;
 			break;
 		}
 	}
 	for (const property in enumObj) {
-		if (isStringEnum || typeof enumObj[property] === "number") {
+		if (isStringEnum || typeof enumObj[property] === 'number') {
 			yield enumObj[property];
 		}
 	}
