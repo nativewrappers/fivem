90d8ef5bc9c39f30fe8fba1a415d4de6d479cf2d
AvarianKnight <dillon@dillonskaggs.dev>
2024-03-10 21:24:01 -0500

    fix(ci): switch bump-package-version to use module syntax
    - rename "package" -> "packageFile" as "package" is reserved

--- .github/actions/bump-package-version.js
+++ .github/actions/bump-package-version.js
@@ -1,13 +1,12 @@
-const fs = require('fs')
+import fs from "node:fs"
 
 const newVersion = process.env.RELEASE_VERSION;
 if (!newVersion) throw new Error("Couldn't get the new version");
 
-let package = fs.readFileSync("package.json", {encoding: 'utf8'});
-package = JSON.parse(package);
-package.version = newVersion;
+let packageFile = fs.readFileSync("package.json", {encoding: 'utf8'});
+packageFile = JSON.parse(packageFile);
+packageFile.version = newVersion;
 
-package = JSON.stringify(package, null, 2);
+packageFile = JSON.stringify(packageFile, null, 2);
 
-fs.writeFileSync("package.json", package);
-
+fs.writeFileSync("package.json", packageFile);
