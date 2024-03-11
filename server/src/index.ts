9810dfc4b4e450ffc0aff59ac26b702449633011
Linden <65407488+thelindat@users.noreply.github.com>
2024-03-11 18:05:09 +1100

    fix: install tsc-alias and add to build script

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ export * from './utils';
 export * from './Events';
 export * from './Game';
 
-export { Convar, Kvp, Resource } from '../../common/index';
+export { Convar, Kvp, Resource } from '@common/index';
 
 export * from './utils/index';
 export * from './entities/index';
