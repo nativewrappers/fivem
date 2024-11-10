a5ce9264435408bb96dc439b8c6496572d809c39
AvarianKnight <dillon@dillonskaggs.dev>
2024-11-09 21:30:47 -0600

    fix(decors): fix bad implementation

--- src/common/decors/Events.ts
+++ src/common/decors/Events.ts
@@ -13,63 +13,59 @@ export enum ConVarType {
   Boolean
 }
 
-type ConVarDefault = number | boolean | undefined;
 
-type OptionalData = {
-  // if the type is a floating point number for convar instead of just a regular
-  // number
-  is_floating_point?: boolean;
-  // if this is NetEvent this will only allow the event to be triggered from
-  // remote, `emit` will not be able to trigger it.
-  remote_only?: boolean;
+export function Exports(exportName: string) {
+  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
+    function replacementMethod(this: any, ...args: any[]) {
+      const t = this;
+
+      exports(exportName, (...args: any[]) => {
+        return originalMethod.call(t, ...args);
+      });
+    }
+
+    return replacementMethod;
+  }
 }
 
-type DecoratorData = {
-  name: string;
-  key: string;
-  type: DecoratorType;
-} & OptionalData;
 
-const update_decor_data = (
-  target: any,
-  name: string,
-  key: string,
-  type: DecoratorType,
-  options?: OptionalData
-) => {
-  Reflect.defineProperty(target, "native_decor", { value: [] });
+export function Event(eventName: string) {
+  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
+    context.addInitializer(function () {
+      const t = this;
+      on(eventName, (...args: any[]) => {
+        return originalMethod.call(t, ...args);
+      });
+    })
+  }
+}
 
-  const data = Reflect.get(target, "native_decor") as DecoratorData[];
-  data.push({
-    name,
-    key,
-    type,
-    ...options
-  });
-};
+export function NetEvent(eventName: string, remoteOnly = false) {
+  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
+    context.addInitializer(function () {
+      const t = this;
+      onNet(eventName, (...args: any[]) => {
+        CLIENT: {
+          if (remoteOnly && source !== 65535) {
+            return;
+          }
+        }
+        return originalMethod.call(t, ...args);
+      });
+    })
+  }
+}
 
-export const Exports = (exportName: string) => {
-  return function (target: any, key: string): void {
-    update_decor_data(target, exportName, key, DecoratorType.Export);
-  };
-};
+export function NuiEvent(eventName: string) {
+  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
+    context.addInitializer(function () {
+      const t = this;
 
-export const Event = (eventName: string) => {
-  return function (target: any, key: string): void {
-    update_decor_data(target, eventName, key, DecoratorType.Event);
-  };
-};
-
-export const NetEvent = (eventName: string, remoteOnly = false) => {
-  return function (target: any, key: string): void {
-    update_decor_data(target, eventName, key, DecoratorType.NetEvent, { remote_only: remoteOnly });
-  };
-};
-
-export const NuiEvent = (eventName: string) => {
-  return function (target: any, key: string): void {
-    update_decor_data(target, eventName, key, DecoratorType.NuiEvent);
-  };
+      RegisterNuiCallback(eventName, (...args: any[]) => {
+        return originalMethod.call(t, ...args);
+      });
+    })
+  }
 };
 
 type ConVarFunction = (convarName: string, defaultValue: any) => any;
@@ -87,79 +83,40 @@ const get_convar_fn = (con_var_type: ConVarType): ConVarFunction => {
 }
 
 
-// NOTE: The default value of the value this is set on will be used as
-// `default_value` for gets
-export const ConVar = (name: string, is_floating_point?: boolean) => {
-  return function (target: any, key: string): void {
-    update_decor_data(target, name, key, DecoratorType.NuiEvent, { is_floating_point });
+export function ConVar(name: string, is_floating_point?: boolean) {
+  return function actualDecorator(_initialValue: any, context: ClassMethodDecoratorContext) {
+    context.addInitializer(function () {
+      const t = this as any;
 
-  };
-};
-
-export const CfxDecors = function () {
-  return function <T extends { new(...args: any[]): any }>(constructor: T) {
-    return class extends constructor {
-      constructor(...args: any[]) {
-        super(...args);
-
-        Reflect.defineProperty(this, "native_decor", { value: [] });
-
-        const events = Reflect.get(this, "native_decor") as Array<DecoratorData>;
-
-        for (const { name, key, type, remote_only, is_floating_point } of events) {
-          switch (type) {
-            case DecoratorType.NuiEvent: {
-              RegisterNuiCallback(name, (...args: any[]) => {
-                this[key](...args);
-              });
-              continue;
-            }
-            case DecoratorType.Event: {
-              on(name, (...args: any[]) => {
-                this[key](...args);
-              });
-              continue;
-            }
-            case DecoratorType.NetEvent: {
-              onNet(name, (...args: any[]) => {
-                CLIENT: {
-                  if (remote_only && source !== 65535) {
-                    return;
-                  }
-                }
-                this[key](...args);
-              });
-              continue;
-            }
-            case DecoratorType.Export: {
-              exports(name, (...args: any[]) => {
-                return this[key](...args);
-              });
-            }
-
-            case DecoratorType.ConVar: {
-              const default_value = this[key];
-              const default_type = typeof default_value;
-              let con_var_type = ConVarType.String
-              if (default_type == "number") {
-                if (is_floating_point) {
-                  con_var_type = ConVarType.Float;
-                } else {
-                  con_var_type = ConVarType.Integer;
-                }
-              } else if (default_type == "boolean") {
-                con_var_type = ConVarType.Boolean;
-              }
-              const con_var_fn = get_convar_fn(con_var_type!);
-              Reflect.set(this, key, con_var_fn(name, default_value))
-              AddConvarChangeListener(name, (con_var_name: string) => {
-                Reflect.set(this, key, con_var_fn(con_var_name, default_value))
-              })
-            }
-          }
+      const default_value = t[context.name];
+      const default_type = typeof default_value;
+      let con_var_type = ConVarType.String
+      if (default_type == "number") {
+        if (is_floating_point) {
+          con_var_type = ConVarType.Float;
+        } else {
+          con_var_type = ConVarType.Integer;
         }
+      } else if (default_type == "boolean") {
+        con_var_type = ConVarType.Boolean;
       }
-    };
-  };
+
+      const con_var_fn = get_convar_fn(con_var_type!);
+      Reflect.set(t, context.name, con_var_fn(name, default_value))
+      AddConvarChangeListener(name, (con_var_name: string) => {
+        Reflect.set(t, context.name, con_var_fn(con_var_name, default_value))
+      })
+    })
+  }
 };
 
+export function SetTick() {
+  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
+    context.addInitializer(function () {
+      const t = this;
+      setTick(async () => {
+        await originalMethod.call(t);
+      })
+    })
+  }
+}
